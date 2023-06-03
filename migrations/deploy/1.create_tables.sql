44918fa89e1e1b62e83aee536e1d883ab3e91e06
EtiamSani <etiam.sani@laposte.net>
2023-06-03 15:53:44 +0200

    Modify Regex

--- migrations/deploy/1.create_tables.sql
+++ migrations/deploy/1.create_tables.sql
@@ -24,7 +24,7 @@ CHECK(
 --Creation of domain using REGEX for emails.
 CREATE DOMAIN "email" AS text
 CHECK(
-    value ~ '(?:[a-z0-9!#$%&''*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])'
+    value ~ '(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)'
 );
 
 -- Creation of domain using REGEX for names & surnames + city names
